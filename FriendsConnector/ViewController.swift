2d847097cb79e502f500b89c070fb96864ada55d
Freddy <fredericdouamba2014@gmail.com>
2023-12-27 07:13:30 -0800

    Update git

--- FriendsConnector/ViewController.swift
+++ FriendsConnector/ViewController.swift
@@ -11,8 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        setupSegmentedControl()
+        setupCollectionView()
+        setupTabBar()
     }
+
+    func setupNavigationBar() {
+        // Set up your navigation bar with a title and search bar
+    }
+
+    func setupSegmentedControl() {
+        // Set up your segmented control for categories like 'Music', 'Sports', etc.
+    }
+
+    func setupCollectionView() {
+        // Set up your collection view to display event cards
+    }
+
+    func setupTabBar() {
+        // Set up your tab bar with different sections like Home, Search, etc.
+    }
+    
+    
+
 
 
 }
